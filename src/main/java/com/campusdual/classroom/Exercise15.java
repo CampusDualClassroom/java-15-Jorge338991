8319efd6b580a69a70672f0a6b6024c965a0c2f1
Jorge338991 <jorge338991@gmail.com>
2024-12-06 22:22:02 +0100

    Se añade solución a Exercise15.java

--- src/main/java/com/campusdual/classroom/Exercise15.java
+++ src/main/java/com/campusdual/classroom/Exercise15.java
@@ -1,15 +1,14 @@
 package com.campusdual.classroom;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class Exercise15 {
 
-	public static void main(String[] args) {
-		FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
-				Calendar.getInstance().getTime());
-		fm.printSpecificData();
-		System.out.println(fm.getLocation());
-
-	}
-
+    public static void main(String[] args) {
+        Calendar c1 = Calendar.getInstance();
+        c1.set(23, 04, 04);
+        FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53, c1.getTime());
+        fm.printSpecificData();
+    }
 }
--- src/main/java/com/campusdual/classroom/Merchandise.java
+++ src/main/java/com/campusdual/classroom/Merchandise.java
@@ -2,97 +2,97 @@ package com.campusdual.classroom;
 
 public abstract class Merchandise {
 
-	protected String name;
-	protected String uniqueId;
-	protected String responsibleId;
-	protected int zone;
-	protected String area;
-	protected String shelf;
-	protected int quantity;
+    protected String name;
+    protected String uniqueId;
+    protected String responsibleId;
+    protected int zone;
+    protected String area;
+    protected String shelf;
+    protected int quantity;
 
-	public Merchandise(String name, String uniqueId, String responsibleId) {
-		this.name = name;
-		this.uniqueId = uniqueId;
-		this.responsibleId = responsibleId;
-	}
+    public Merchandise(String name, String uniqueId, String responsibleId) {
+        this.name = name;
+        this.uniqueId = uniqueId;
+        this.responsibleId = responsibleId;
+    }
 
-	public Merchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf,
-			int quantity) {
-		this.name = name;
-		this.uniqueId = uniqueId;
-		this.responsibleId = responsibleId;
-		this.zone = zone;
-		this.area = area;
-		this.shelf = shelf;
-		this.quantity = quantity;
-	}
+    public Merchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf,
+                       int quantity) {
+        this.name = name;
+        this.uniqueId = uniqueId;
+        this.responsibleId = responsibleId;
+        this.zone = zone;
+        this.area = area;
+        this.shelf = shelf;
+        this.quantity = quantity;
+    }
 
-	public String getLocation() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Z - ");
-		builder.append(getZone());
-		builder.append(" A - ");
-		builder.append(getArea());
-		builder.append(" E - ");
-		builder.append(getShelf());
-		return builder.toString();
-	}
+    public String getLocation() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Z - ");
+        builder.append(getZone());
+        builder.append(" A - ");
+        builder.append(getArea());
+        builder.append(" E - ");
+        builder.append(getShelf());
+        return builder.toString();
+    }
 
-	public abstract Object getSpecificData();
+    public abstract Object getSpecificData();
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getUniqueId() {
-		return uniqueId;
-	}
+    public String getUniqueId() {
+        return uniqueId;
+    }
 
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
 
-	public String getResponsibleId() {
-		return responsibleId;
-	}
+    public String getResponsibleId() {
+        return responsibleId;
+    }
 
-	public void setResponsibleId(String responsibleId) {
-		this.responsibleId = responsibleId;
-	}
+    public void setResponsibleId(String responsibleId) {
+        this.responsibleId = responsibleId;
+    }
 
-	public int getZone() {
-		return zone;
-	}
+    public int getZone() {
+        return zone;
+    }
 
-	public void setZone(int zone) {
-		this.zone = zone;
-	}
+    public void setZone(int zone) {
+        this.zone = zone;
+    }
 
-	public String getArea() {
-		return area;
-	}
+    public String getArea() {
+        return area;
+    }
 
-	public void setArea(String area) {
-		this.area = area;
-	}
+    public void setArea(String area) {
+        this.area = area;
+    }
 
-	public String getShelf() {
-		return shelf;
-	}
+    public String getShelf() {
+        return shelf;
+    }
 
-	public void setShelf(String shelf) {
-		this.shelf = shelf;
-	}
+    public void setShelf(String shelf) {
+        this.shelf = shelf;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
